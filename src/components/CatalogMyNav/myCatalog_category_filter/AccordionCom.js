2cab2d4bab50a10254108b101da86f8899cbd65a
samy246 <sdurai246@gmail.com>
2023-04-11 20:41:54 +0530

    filter commit

--- src/components/CatalogMyNav/myCatalog_category_filter/AccordionCom.js
+++ src/components/CatalogMyNav/myCatalog_category_filter/AccordionCom.js
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import './AccordionCom.scss'
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 
 function AccordionCom ({ data, handler, type, clear }) {
+
+
+  const [isRadiocat, setIsRadiocat] = useState("");
+  const [isRadiosubcat, setIsRadiosubcat] = useState([]);
 //console.log("7all subcategory",allSubCategory);
   const { t } = useTranslation();
   var { subCat } = useParams();
@@ -14,6 +18,17 @@ function AccordionCom ({ data, handler, type, clear }) {
   // console.log("a params",a.id);
 console.log("subcat12=====================>",subCat);
 console.log("Accord data",data);
+
+
+
+
+
+
+
+
+
+
+
   useEffect(() => {
     if(subCat === undefined) {
       return
@@ -25,7 +40,7 @@ console.log("Accord data",data);
         console.log(sub)
       }
       if(type === "Filter By") {
-        let sub = data?.find(d => d?.id == subCat)
+        let sub = data?.find(d => d?.id == idparams.id)
         console.log("subidd==>",sub);
         handler(sub?.id)
         console.log(sub)
@@ -84,8 +99,10 @@ console.log("Accord data",data);
                     className="form-check-input"
                     name={type}
                     // {type === "SubCategory"}
-                    checked={itm?.id === idparams.id }
+                    //checked={ idparams.id ==itm?.id }
                     //checked={itm?.id === "1064"}
+                    //checked={itm?.id == idparams.id}
+                   // checked={itm?.id == idparams.id}
                     value={itm?.product_type_code ? itm?.product_type_code : itm?.short_code ? itm?.short_code : itm?.country_id ? itm?.country_id : itm?.id}
                     id={itm?.product_type_code ? itm?.product_type_code : itm?.short_code ? itm?.short_code : itm?.country_id ? itm?.country_id : itm?.id}
                     onClick={() => handler(itm?.product_type_code ? itm?.product_type_code : itm?.short_code ? itm?.short_code : itm?.country_id ? itm?.country_id : itm?.id)}
@@ -109,7 +126,7 @@ console.log("Accord data",data);
 
 {/* another data sub category*/}
 
-{/* <div className="accordion-item"> */}
+<div className="accordion-item">
 
       <div
         id={`flush-collapse${itm?.id}`}
@@ -139,7 +156,8 @@ console.log("Accord data",data);
                     name={type}
                     // {type === "SubCategory"}
                     //checked={itm?.id === a.id}
-                    checked={item.id === idparams.subCat }
+                    //checked={ idparams.subCat === item.id  }
+                   // checked={ item?.id == idparams.subCat}
                     value={item?.product_type_code ? item?.product_type_code : item?.short_code ? item?.short_code : item?.country_id ? item?.country_id : item?.id}
                     id={item?.product_type_code ? item?.product_type_code : item?.short_code ? item?.short_code : item?.country_id ? item?.country_id : item?.id}
                     onClick={() => handler(item?.product_type_code ? item?.product_type_code : item?.short_code ? item?.short_code : item?.country_id ? item?.country_id : item?.id)}
@@ -164,7 +182,7 @@ console.log("Accord data",data);
           </div>
         </div>
       </div>
-    {/* </div> */}
+    </div>
             
 
 
